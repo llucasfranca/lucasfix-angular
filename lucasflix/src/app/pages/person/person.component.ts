c78c4b266c99ede4c4c40fa3c98b45e7c953ddde
llucasfranca <lucasgt@hotmail.com>
2025-06-06 22:33:03 -0300

    ooop

---
+++ lucasflix/src/app/pages/person/person.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person',
+  imports: [CommonModule],
+  templateUrl: './person.component.html',
+  styleUrl: './person.component.css'
+})
+export class PersonComponent {
+
+}
